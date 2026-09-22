ba272b94091894c5d045ba638c878c6961710431
devchicala <delciofrancisco_1@gmail.com>
2026-09-23 00:24:44 +0100

    feat: opportunity repo delete

--- src/main/java/com/example/rest_api/adapter/out/repository/OportunityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rest_api.adapter.out.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OportunityRepository extends JpaRepository <Opportunity, Long> {
-
-}
